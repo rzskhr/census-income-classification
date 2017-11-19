536e6ceade2412f95d1211b2a5305a1d7026eb52
Raj Dutta <rzskhr@outlook.com>
2017-11-18 23:37:38 -0600

    Test model

--- sandbox/test.py
+++ sandbox/test.py
@@ -1,29 +1,33 @@
 print("Hello, World!")
-
-model_selection = 0
+from collections import defaultdict
 
 class ModelMetrics(object):
 
     # Random permutation cross-validator with 80-20 train test split
-    cv = model_selection.ShuffleSplit(n_splits = 10, test_size = 0.2)
+    cv = ShuffleSplit(n_splits = 2, test_size = 0.2) # change this to 10
     # dictionary to store scores
-    model_scores = defaultdict(list)
+    model_scores = defaultdict(lambda:None)
+    # default scoring metrics
+    default_metric = ['accuracy','precision', 'recall']
 
     def __init__(self, model_name, model_obj, features, response):
-        self.model_name = model_name
+        self.model = model_name
         self.model_obj = model_obj
-        ModelMetrics.model_scores[model_name] = list()
+        ModelMetrics.model_scores[model_name] = []
         self.cv = ModelMetrics.cv
         self.features = features
         self.response = response
+        self.model_scores = ModelMetrics.model_scores
 
-    def model_scoring(self, scoring_metric):
+    def model_scoring(self, scoring_metric=default_metric):
         for metric in scoring_metric:
-            n_fold_score = cross_val_score(self.model_obj, self.features,
+            n_fold_score = cross_val_score(self.model_obj,self.features,
                                                            self.response,
                                                            cv=self.cv,
                                                            scoring=metric)
-            model_scores[self.model_name].append({metric:np.mean(n_fold_score)})
-
+            mean_score = np.mean(n_fold_score)
+            self.model_scores[self.model].append({metric:mean_score})
+        model_scores = self.model_scores
+        return model_scores
 
 
